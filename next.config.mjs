8cb99007fafcd80e25d2c04d8272ee215e1b23e0
BrianWahungu <brianwahungu123@gmail.com>
2026-06-25 16:36:33 +0300

    netlifyDevOps

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <-- Add this line
+  // Leave any other existing settings below if you have them
+};
 
-export default nextConfig;
+module.exports = nextConfig;
